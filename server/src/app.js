a762c53969d95fc464281f0c0c5e8b9cd2b1e478
senqui <hcquindao@up.edu.ph>
2026-05-17 22:52:12 +0800

    [FIX] Notes on Render not creating

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const notesRouter = require("./routes/notes");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://hopecore-client.onrender.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.use(express.json());
 app.use("/api/notes", notesRouter);
 
