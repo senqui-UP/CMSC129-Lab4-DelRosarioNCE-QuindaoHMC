0dbc64bbedbefb775d4249321adbcf36569bafec
senqui <hcquindao@up.edu.ph>
2026-05-17 13:31:09 +0800

    [REFACTOR] Minor ceanup in codebase
    - changed server running on port in server/src/index.js
    - removed import React in client/src/main.jsx

--- server/src/index.js
+++ server/src/index.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:5173`)
+  console.log(`Server running on http://localhost:${PORT}`)
 })
